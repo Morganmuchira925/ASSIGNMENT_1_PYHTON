3efdec67f540d9fa63244a6f992d7d4bf148ee0e
Morgan <morganisrael2005@gmail.com>
2024-08-21 11:17:10 +0300

    Assignment_1_PLP

---
+++ greet_module.py
@@ -0,0 +1,6 @@
+# Define Functions
+def greet(person):
+    return f"Good Afternoon, {person}"
+#Apply the Function
+print(greet("John"))
+print(greet("Morgan"))
---
+++ plp_Academy.py
@@ -0,0 +1,61 @@
+# # favourite number
+# favourite_number = 17
+# print(f'My favourite number is {favourite_number}')
+# #String
+# greeting = 'Olla PLP!'
+# print(greeting)
+# # Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f"Is Python fun? {is_python_fun}")
+# print(f"Is Python boring? {is_python_boring}")
+
+# #Control FLow
+# speed = 80
+# if speed > 69:
+#     print("The speed is fast")
+#     if speed > 45:
+#         print("The speed is slow")
+#     else:
+#         print("The speed is normal")
+
+#Loops(for loop, while loop )
+# for i in range(8):
+#     print(f'I love Plp Academy {i}')
+# #while
+# CountDown = 10
+# while CountDown > 0:
+#     print(f"The countDown is: {CountDown}")
+#     CountDown -= 1
+#     print("CountDown is successful!!!")
+
+# Define Functions
+# def greet(person):
+#     return f"Good Afternoon, {person}"
+# #Apply the Function
+# print(greet("John"))
+# print(greet("Morgan"))
+
+#Lists and Dictionaries(tuples)
+# fruits_list = ["Apple", "Mango", "Watermelon", "Peas", "Cherry"]
+# fruits_tuple = ("Banana", "Pineapple", "Oranges")
+# print(fruits_list)
+# print(fruits_tuple)
+
+#Dictionary
+# Contacts = {
+#     "John": "1234567890",
+#     "Doe": "01152566325",
+#     "Allan": "011362634"
+# }
+# print(f"Doe contact is {Contacts['Doe']}")
+
+#Inbuilt Modules in Python
+#import math
+#Use a Function fro Module Math
+#result = math.sqrt(16)
+#print(f'Square Root of 16 is {result}')
+
+#Customized module
+# import greet_module
+# print(greet_module.greet('Morgan'))
